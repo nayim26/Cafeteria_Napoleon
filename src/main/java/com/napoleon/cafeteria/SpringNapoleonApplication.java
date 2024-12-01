60cf8e0e8886d8a246b851612695f3b13ccccccb
Melany Aliaga Gomez <miyan30mel10@gmail.com>
2024-11-30 19:41:07 -0500

    creacion de la estructura del proyecto

---
+++ src/main/java/com/napoleon/cafeteria/SpringNapoleonApplication.java
@@ -0,0 +1,13 @@
+package com.napoleon.cafeteria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringNapoleonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringNapoleonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/napoleon/cafeteria/SpringNapoleonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.napoleon.cafeteria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringNapoleonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
