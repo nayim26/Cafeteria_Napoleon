5af02bb8acc5cf7d237e1aee3ec2bd5b1395de05
Melany Aliaga Gomez <miyan30mel10@gmail.com>
2024-12-01 00:58:17 -0500

    funcionalidad para editar productos

--- src/main/java/com/napoleon/cafeteria/controller/ProductoController.java
+++ src/main/java/com/napoleon/cafeteria/controller/ProductoController.java
@@ -1,11 +1,14 @@
 package com.napoleon.cafeteria.controller;
 
+import java.util.Optional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model; 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -41,4 +44,24 @@ public class ProductoController {
         productoService.save(producto);
         return "redirect:/productos";
     }
+    
+	@GetMapping("/edit/{id}")
+	public String edit(@PathVariable Integer id, Model model) {
+		Producto producto= new Producto();
+		Optional<Producto> optionalProducto=productoService.get(id);
+		producto= optionalProducto.get();
+		
+		LOGGER.info("Producto buscado: {}",producto);
+		model.addAttribute("producto", producto);
+		
+		return "productos/edit";
+	}
+	
+	@PostMapping("/update")
+	public String update(Producto producto) {
+		productoService.update(producto);		
+		return "redirect:/productos";
+	}
+	
+    
 }
